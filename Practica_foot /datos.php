de35fa55f053ca4e5ef46a07402f17a89b6cd056
Alejandro98-10 <2430305@upv.edu.mx>
2025-11-22 00:01:31 -0600

    add new practice and process

---
+++ Practica_foot /datos.php
@@ -0,0 +1,12 @@
+<?php
+
+$nombre = $_REQUEST['nombre'];
+$apellido = $_REQUEST['apellido'];
+$telefono = $_REQUEST['telefono'];
+$sexo = $_REQUEST['sexo'];
+$correo = $_REQUEST['correo'];
+$equipo = $_REQUEST['equipo'];
+$dorsal = $_REQUEST['dorsal'];
+
+
+echo"el nombre es: $nombre , el apellido es: $apellido ,el telefono es: $telefono , el sexo es: $sexo , el correo es: $correo , el equipo es: $equipo y el dorsal es $dorsal";
